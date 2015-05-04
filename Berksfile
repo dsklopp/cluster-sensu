bb986b1391830af57b7b85d9bbab61efe272dbbc
git <dsklopp@gmail.com>
2015-05-03 17:10:06 -0700

    Moving dashboard around

--- Berksfile
+++ Berksfile
@@ -1,6 +1,7 @@
 source "https://api.berkshelf.com"
 
 cookbook 'guru-sensu', git: 'https://github.com/dsklopp/guru-sensu.git'
+#cookbook 'guru-sensu', path: '../guru-sensu'
 cookbook 'consul-template', git: 'https://github.com/adamkrone/chef-consul-template.git'
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name             'cluster-sensu'
 maintainer       'Daniel Klopp'
-maintainer_email 'Submit an issue, I'll see it'
+maintainer_email 'none'
 license          'All rights reserved'
 description      'Installs/Configures cluster-sensu'
 long_description 'Installs/Configures cluster-sensu'
 version          '0.1.0'
 
 depends 'guru-sensu'
+
